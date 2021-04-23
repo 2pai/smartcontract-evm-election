d91218137549eb3146854fec2f6968c6dbd5ac12
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-04-23 18:43:48 +0700

    Add massDelegateVote & comment to improve Docs
    - add massDelegateVote function for delegate vote to mass user (using array of address params)
    - add comment to every function & event for better clarity
    - cleanup bad vars/function/event naming for better clarity
    
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.8.0;
 
+/// @author Muhammad Iqbal Syamil Ayasy
+/// @title An example election smart contract (EVM)
 contract Voting{
 
-    bool public voteActive; // status voting 
+    /// Emit Winner of election when `endVoting` was called
+    /// @param winnerCandidate name of candidate
+    /// @param totalCount total ballot of candidate 
+    event Winner(string winnerCandidate, uint totalCount);
+
+    bool public isVoteActive; // status voting 
     address public owner; // address deployer
-    uint public epoch; // end of voting epoch
+    uint public endEpoch; // end of voting epoch
+
     struct Voter {
         bool voted;  // if true, that person already voted
         bool eligible; // person delegated to
@@ -22,16 +30,17 @@ contract Voting{
         bool validCandidate; // is a valid candidate or not
     }
 
-    // stores a `Voter` struct for each possible address.
+    /// stores a `Voter` struct for each possible address.
     mapping(address => Voter) public voters;
 
-    // A dynamically-sized array of `candidate` structs.
+    /// A dynamically-sized array of `candidate` structs.
     Candidate[] public candidate;
-
+    
+    /// Init Contract by set owner = msg.sender, set end period & init candidate
     constructor () public {
-        voteActive = true; 
+        isVoteActive = true; 
         owner = msg.sender; // set owner contract = deployer
-        epoch = block.timestamp + 259200; // set voting active (+3 Days) after Contract Deployed
+        endEpoch = block.timestamp + 259200; // set voting active (+3 Days) after Contract Deployed
         
         // list candidate (order = index)
         candidate.push(Candidate(
@@ -39,6 +48,7 @@ contract Voting{
             0,
             true
         ));
+
         candidate.push(Candidate(
             'Syamil',
             0,
@@ -47,31 +57,51 @@ contract Voting{
         
     }
 
-    // set the voting period to end (require epoch <= current block timestamp)
+    /// set the voting period to end (require epoch <= current block timestamp)
+    /// @dev when the winner is picked, emit `Winner`event
     function endVoting() public onlyOwner {
-        require(epoch <= block.timestamp, "Epoch are not less than current block.timestamp");
-        voteActive = false;
+        require(endEpoch <= block.timestamp, "Epoch are not less than the current block.timestamp");
+
+        uint largest = 0;
+        uint winnerCandidate;
+        for (uint256 i = 0; i < candidate.length; i++) {
+            if(candidate[i].voteCount > largest){
+                winnerCandidate = i;
+            }
+        }
+
+        emit Winner(candidate[winnerCandidate].name, candidate[winnerCandidate].voteCount);
+        isVoteActive = false;
     }
 
-    // delegate vote to voters address, can only accesible from owner; 
-    function delegateVote(address addressVoter) public onlyOwner {
-        require(!voters[addressVoter].voted,"The Address Is already on the List");
-        Voter memory vote = voters[addressVoter];
-        vote.voted = false;
-        vote.eligible = true;
+    /// delegate vote to voters address, can only accesible from owner
+    /// @param voterAddress address of voter to delegate vote
+    function delegateVote(address voterAddress) public onlyOwner {
+        require(!voters[voterAddress].voted,"The Address Is already on the List");
+        voters[voterAddress].voted = false;
+        voters[voterAddress].eligible = true;
 
+    }
+    /// mass delegate vote to list voters address, can only accesible from owner
+    /// @param listVoterAddress list of address of voter to delegate vote
+    function massDelegateVote(address[] memory listVoterAddress) public onlyOwner {
+        for (uint256 p = 0; p < listVoterAddress.length; p++) {
+            delegateVote(listVoterAddress[p]);
+        }
     }
     
-    // function to vote, takes candidate number (index) as params
-    function vote(uint candidateNumber) public {
+    /// vote the candidate
+    /// @param numberCandidate index of candidate
+    /// @dev increment the state of `voteCount` on candidate struct by +1
+    function vote(uint numberCandidate) public {
 
-        require(voteActive, "Voting period Has Ended."); // check voting period is still active
+        require(isVoteActive, "Voting period Has Ended."); // check voting period is still active
         require(voters[msg.sender].eligible, "You're Not eligible to vote"); // check if voter are eligible
         require(!voters[msg.sender].voted, "The voter already voted."); // check if voters has not voted yet
-        require(candidate[candidateNumber].validCandidate,"Candidate Number is Invalid"); // check candidate 
+        require(candidate[numberCandidate].validCandidate,"Candidate Number is Invalid"); // check candidate 
         
-        candidate[candidateNumber].voteCount = candidate[candidateNumber].voteCount + 1;
-        voters[msg.sender].vote = candidateNumber;
+        candidate[numberCandidate].voteCount = candidate[numberCandidate].voteCount + 1;
+        voters[msg.sender].vote = numberCandidate;
         voters[msg.sender].voted = true; 
             
         
