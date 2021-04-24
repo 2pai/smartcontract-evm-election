198d5ad8f67c8ae5a89acafa26ffe29260133f3f
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-04-25 05:03:34 +0700

    migrations: add listCandidate when the contract is deployed for initializing candidate
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const Voting = artifacts.require("Voting");
-
+const { CANDIDATE } = require('./constant')
 module.exports = function (deployer) {
-  deployer.deploy(Voting);
+  deployer.deploy(Voting, CANDIDATE);
 };
---
+++ migrations/constant.js
@@ -0,0 +1,5 @@
+const CANDIDATE = ['Alice','Bob','Iqbal']
+
+module.exports = {
+    CANDIDATE
+}
